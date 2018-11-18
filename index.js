c6c389db85eeb4eadb3d8e5c3920df892baf7aa6
Olivia Kasten <odkasten@gmail.com>
2018-11-18 16:43:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function removeLastDriver(){
 }
 
 function removeFirstDriver(){
-  const newArray = [drivers.slice(0, 1), ...drivers];
+  const newArray = drivers.splice(0, 1);
   return newArray;
 }
 
